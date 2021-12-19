ab724cabd97baee909778656749f456777fef123
chengen0612 <chengen0612@gmail.com>
2021-12-19 11:18:15 +0800

    log: add counter to the app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./index.css";
 
 console.log("hello world!");
+
+const counter = document.getElementById("counter");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  let number = parseInt(counter.innerHTML, 10);
+  counter.innerHTML = ++number;
+});
