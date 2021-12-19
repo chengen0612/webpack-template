0ddbe7aa6bd1e33dbdf71e0d7596b7396546b038
chengen0612 <chengen0612@gmail.com>
2021-12-19 18:13:15 +0800

    log: add es10 syntax to index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@ btn.addEventListener("click", function () {
   let number = parseInt(counter.innerHTML, 10);
   counter.innerHTML = ++number;
 });
+
+// ES10 syntax
+class Foo {
+  #privateMsg = "I am private!";
+}
+
+const foo = new Foo();
+
+console.log("foo.privateMsg", foo.privateMsg);
